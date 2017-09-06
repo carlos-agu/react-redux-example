eeebc060be77ac0360ffd6458148b868986104c0
Carlos Aguirre <carlos.agu@gmail.com>
2017-09-06 14:56:39 -0600

    Primer Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'JavaScript: The good parts'},
+        {title: 'Harry Potter And the half blooded prince'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
