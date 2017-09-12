1e573cf647541d8e98a52f889b0a9b2ff49b14d8
Carlos Aguirre <carlos.agu@gmail.com>
2017-09-12 15:00:24 -0600

    Se completo lo de redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -21,5 +21,9 @@ class BookList extends Component{
 }
 
 function mapStateToProps(state){
-    
+    return {
+        books: state.books
+    }    
 }
+
+export default connect(mapStateToProps)(BookList);
